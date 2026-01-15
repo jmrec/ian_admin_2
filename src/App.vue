441e8b5da2cc286fe8c09ec6489436fa436f5e07
Juan Miguel Recondo <134484809+jmrec@users.noreply.github.com>
2026-01-16 04:52:02 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,110 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import SideBar from './components/SideBar.vue'
+</script>
+
+<template>
+
+  <body class="bg-gray-50 dark:bg-gray-900 font-display text-gray-800 dark:text-gray-200 overflow-hidden">
+
+    <!-- <header>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </header> -->
+
+    <div class="flex h-screen w-full">
+      <SideBar />
+      <RouterView class="flex-1 flex flex-col h-screen overflow-hidden relative bg-gray-50 dark:bg-gray-900 z-0">
+        <header class="h-16 bg-transparent flex justify-between items-center px-8 py-4 shrink-0">
+
+        </header>
+        <main class="flex-1 overflow-y-auto px-8 pb-8 scroll-smooth"></main>
+      </RouterView>
+    </div>
+  </body>
+</template>
+
+<style>
+.material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
+
+<!-- <style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style> -->
